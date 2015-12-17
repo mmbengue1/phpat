b18f544653d57d9ec8c43d0ff7cd395e9c0f1178
mmbengue <kingmbengue@gmail.com>
2015-12-17 11:31:33 -0500

    yhtyh

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmbengue
+ * Date: 2015-12-17
+ * Time: 11:15
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>site_chat</title>
+    <h1>site_chat</h1>
+</head>
+<body>
+
+</body>
+</html>
